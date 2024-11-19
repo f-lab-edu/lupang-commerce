9605deb9537a22615c759cadb7ab393bdd4b6e34
jihoonyou <dbwlgna98@naver.com>
2024-11-19 16:08:03 +0900

    [LC-1] 프로젝트 초기설정 init

---
+++ lupang-commerce-admin/src/main/java/kr/lucorp/lupangcommerceadmin/LupangCommerceAdminApplication.java
@@ -0,0 +1,13 @@
+package kr.lucorp.lupangcommerceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupangCommerceAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LupangCommerceAdminApplication.class, args);
+  }
+
+}
